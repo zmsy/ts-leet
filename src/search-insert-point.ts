eb5ac9cd52f31ab7f6d310cab871e79c25798e2f
Zach Morrissey <zachmor05@gmail.com>
2022-05-03 19:47:12 -0700

    Fix search-insert-point

--- src/search-insert-point.ts
+++ src/search-insert-point.ts
@@ -8,12 +8,14 @@
 export function searchInsert(nums: number[], target: number): number {
   let i = 0;
   let j = nums.length - 1;
-  while (i < j) {
+  while (i <= j) {
     const mid = Math.floor((i + j) / 2);
-    if (nums[mid] < target) {
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
       i = mid + 1;
     } else {
-      j = mid;
+      j = mid - 1;
     }
   }
   return i;
