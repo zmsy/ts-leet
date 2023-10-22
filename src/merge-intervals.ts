277702291357f0171d22fba5c6fbe8682b4dd910
Zach Morrissey <zachmor05@gmail.com>
2023-10-22 14:54:01 -0700

    Update merge-intervals.ts

--- src/merge-intervals.ts
+++ src/merge-intervals.ts
@@ -4,23 +4,27 @@
 
 export function merge(intervals: number[][]): number[][] {
   if (intervals.length < 2) {
+    // base case, return any non-mergeable lists
     return intervals;
   }
-  const ints = intervals.sort((a, b) => a[0] - b[0]);
 
-  let n = ints.length;
-  let i = 1; // start with the second value and check backwards.
+  intervals.sort((a, b) => a[0] - b[0]);
+  // start with i = 1 and always look backwards
+  let i = 1;
+  let n = intervals.length;
   while (i < n) {
-    // check if prior's number is larger than former's
-    if (ints[i - 1][1] >= ints[i][0]) {
-      ints[i - 1][0] = Math.min(ints[i - 1][0], ints[i][0]);
-      ints[i - 1][1] = Math.max(ints[i - 1][1], ints[i][1]);
-      ints.splice(i, 1);
+    // check if large element of last >= smaller element of current
+    if (intervals[i - 1][1] >= intervals[i][0]) {
+      const lower = Math.min(intervals[i - 1][0], intervals[i][0]);
+      const upper = Math.max(intervals[i - 1][1], intervals[i][1]);
+      intervals[i - 1] = [lower, upper];
+      // remove the current interval
+      intervals.splice(i, 1);
       n -= 1;
     } else {
-      i += 1
+      i += 1;
     }
   }
 
-  return ints;
-};
+  return intervals;
+}
