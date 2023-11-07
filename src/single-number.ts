5022072be61002fe6484a2ab4d80dd089f28e774
Zach Morrissey <zachmor05@gmail.com>
2023-11-06 21:06:25 -0800

    single-number

---
+++ src/single-number.ts
@@ -0,0 +1,15 @@
+/**
+ * Given a non-empty array of integers nums, every element appears twice except
+ * for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only
+ * constant extra space.
+ */
+export function singleNumber(nums: number[]): number {
+  let vector = 0;
+  for (const num of nums) {
+    vector ^= num;
+  }
+
+  return vector;
+}
