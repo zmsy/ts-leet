b9cf41190f7b1bff07445219ae2dc8da729d9137
Zach Morrissey <zachmor05@gmail.com>
2023-11-06 20:02:41 -0800

    combination-sum

--- src/combination-sum.ts
+++ src/combination-sum.ts
@@ -8,24 +8,25 @@ export function combinationSum(
   candidates.sort((a, b) => a - b);
   const output: number[][] = [];
 
-  const search = (left: number, path: number[]): void => {
+  const search = (left: number, path: number[], idx: number): void => {
     // base case: we hit zero, so this is a match. add the
     // path to the output.
     if (left === 0) {
-      output.push(path);
+      output.push([...path]);
       return;
     }
 
     // if not there yet, recurse into lower elements.
-    for (const c of candidates.filter((x) => left - x >= 0)) {
-      search(left - c, [...path, c]);
+    for (let i = idx; i < candidates.length; i++) {
+      const c = candidates[i];
+      if (left - c >= 0) {
+        path.push(c);
+        search(left - c, path, i);
+        path.pop();
+      }
     }
   };
 
-  search(target, []);
-  // return a version of the result with only unique outputs.
-  const uniq = Array.from(
-    new Map(output.map((i) => [i.sort().join(), i])).values()
-  );
-  return uniq;
+  search(target, [], 0);
+  return output;
 }
