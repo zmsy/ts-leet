491964b0f2012d77735c09c3a023676bd75e90a0
Zach Morrissey <zachmor05@gmail.com>
2023-11-08 20:12:26 -0800

    word-search-ii

--- src/word-search-ii.ts
+++ src/word-search-ii.ts
@@ -11,6 +11,39 @@ export function findWords(board: string[][], words: string[]): string[] {
     return [];
   }
 
+  // create a trie to store all of the prefixes.
+  const root: TrieNode = {
+    val: "",
+    isWord: false,
+    children: new Map(),
+  };
+
+  // this is ripped directly from my Trie implementation, so that we can easily
+  // insert into the Trie container.
+  const insertPrefix = (word: string): void => {
+    if (!word.length) {
+      return;
+    }
+    let current: TrieNode = root;
+    for (const w of word) {
+      if (!current.children.has(w)) {
+        // if the next letter in the word
+        // doesn't exist as a node in the tree,
+        // create it first.
+        const newNode: TrieNode = {
+          val: w,
+          isWord: false,
+          children: new Map(),
+        };
+        current.children.set(w, newNode);
+      }
+      current = current.children.get(w)!;
+    }
+    current.isWord = true;
+  };
+
+  words.forEach((word) => insertPrefix(word));
+
   const m = board.length;
   const n = board[0].length;
   const coords = (i: number, j: number) => `${i}.${j}`;
@@ -35,24 +68,37 @@ export function findWords(board: string[][], words: string[]): string[] {
     j: number,
     idx: number,
     visited: Set<string>,
-    candidateWords: Array<string>
+    word: string,
+    trieNode: TrieNode
   ): Array<string> => {
     visited.add(coords(i, j));
     // if this doesn't match the next letter, return early.
-    const newWords = candidateWords.filter((w) => w[idx] === board[i][j]);
-    if (newWords.length === 0) {
+    const char = board[i][j];
+    const childNode = trieNode.children.get(char);
+    const newWord = word + char;
+
+    // if this can't be made into a word, then bounce the F outta here
+    if (childNode === undefined) {
       return [];
     }
 
-    // success case, if it's the final letter, return early.
-    const foundWords = newWords.filter((w) => idx === w.length - 1);
+    const foundWords: string[] = childNode?.isWord ? [word] : [];
 
     const neighbors = getNeighbors(i, j).filter(([l, m]) => {
       return !visited.has(coords(l, m));
     });
     for (const [ni, nj] of neighbors) {
-      const words = dfs(ni, nj, idx + 1, new Set([...visited]), newWords);
-      words.forEach((w) => foundWords.push(w));
+      if (childNode.children.has(board[ni][nj])) {
+        const words = dfs(
+          ni,
+          nj,
+          idx + 1,
+          new Set([...visited]),
+          word + board[ni][nj],
+          childNode
+        );
+        words.forEach((w) => foundWords.push(w));
+      }
     }
 
     return foundWords;
@@ -61,10 +107,16 @@ export function findWords(board: string[][], words: string[]): string[] {
   const allWords = new Set<string>();
   for (let x = 0; x < m; x++) {
     for (let y = 0; y < n; y++) {
-      const found = dfs(x, y, 0, new Set(), words);
+      const found = dfs(x, y, 0, new Set(), board[x][y], root);
       found.forEach((x) => allWords.add(x));
     }
   }
 
   return Array.from(allWords);
+}
+
+interface TrieNode {
+  val: string;
+  isWord: boolean;
+  children: Map<string, TrieNode>;
 }
